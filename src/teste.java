387867e861866d421cae89a469499655545164b5
Anderson Machado <anderson361163@gmail.com>
2022-06-25 18:17:00 -0300

    first commit

---
+++ src/teste.java
@@ -0,0 +1,17 @@
+package src;
+/**
+ * teste
+ */
+public class teste {
+
+    public static void main(String[] args) {
+        System.out.println("Teste dos teste");
+        if(1>2){
+
+        }else{
+            
+        }
+
+
+    }
+}
